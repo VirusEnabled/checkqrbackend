e27b412c50c6ef42e872b14f2c08a78a507ba544
jasperrodriguez <jjrosario@puntacana.com>
2023-01-03 17:11:34 -0400

    adding changes in order to integrate carnetization app

--- apps/utils/services/initial_data/application.py
+++ apps/utils/services/initial_data/application.py
@@ -42,6 +42,44 @@ def setup_production_default():
                                     description='confirms the given booking',
                                     url='confirm_booking/'
                                  ))
+    
+    sscc_application_tag = (ApplicationTag.
+                       objects.
+                       get_or_create(name='sscc')[0])
+    sscc = (Application.
+            objects.
+            get_or_create(
+                name='SSCC_PRODUCTION',
+                description='sistema de carnetizacion, \
+                    esta integracion es para probar con los marbetes vehiculares',
+                tag=sscc_application_tag
+                )[0])
+
+    sscc_config = (ApplicationConfiguration.
+                    objects.
+                    get_or_create(
+                        application=sscc,
+                        base_url='https://sscc.puntacana.com',
+                        implements_input_search=True
+
+                    )[0])
+
+    sscc_confirm_url = (ApplicationUrl.
+                        objects.
+                        get_or_create(
+                        name='check_marbete_GET',
+                        application=sscc,
+                        description='carga la informacion del marbete',
+                        url='/marbete/verifica_marbete/'
+                    ))
+    sscc_confirm_url = (ApplicationUrl.
+                        objects.
+                        get_or_create(
+                        name='confirm_booking_POST',
+                        application=sscc,
+                        description='envia un post al url en base a las politicas utilizadas',
+                        url='/marbete/verifica_marbete/'
+                    ))
 
 def setup_develop_default():
     application_tag = (ApplicationTag.
@@ -80,6 +118,44 @@ def setup_develop_default():
                                         application=bookings_dev_fgpc,
                                         description='confirms the given booking',
                                         url='confirm_booking/'
+                                    ))
+
+    sscc_application_tag = (ApplicationTag.
+                       objects.
+                       get_or_create(name='sscc')[0])
+    sscc_dev = (Application.
+                         objects.
+                         get_or_create(
+                                name='SSCC_DEVELOP',
+                                description='sistema de carnetizacion, \
+                                    esta integracion es para probar con los marbetes vehiculares',
+                                tag=sscc_application_tag
+                                )[0])
+
+    sscc_dev_config = (ApplicationConfiguration.
+                                objects.
+                                get_or_create(
+                                    application=sscc_dev,
+                                    base_url='https://sscc-develop.puntacana.com',
+                                    implements_input_search=True
+
+                                )[0])
+
+    sscc_dev_confirm_url = (ApplicationUrl.
+                                     objects.
+                                     get_or_create(
+                                        name='check_marbete_GET',
+                                        application=sscc_dev,
+                                        description='carga la informacion del marbete',
+                                        url='/marbete/verifica_marbete/'
+                                    ))
+    sscc_dev_confirm_url = (ApplicationUrl.
+                                     objects.
+                                     get_or_create(
+                                        name='confirm_booking_POST',
+                                        application=sscc_dev,
+                                        description='envia un post al url en base a las politicas utilizadas',
+                                        url='/marbete/verifica_marbete/'
                                     ))
 
 
